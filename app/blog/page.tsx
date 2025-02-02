be8d97d76b1a803430bbb418937bdcabf7df06cb
rachmani121 <gbirachmani228@gmail.com>
2025-02-02 17:10:57 +0700

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,8 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'PP',
+  description: 'PPPP.',
 }
 
 export default function Page() {
